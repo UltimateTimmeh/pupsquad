3ca8f14cf8b445c480d6251c81574759dd89cf38
Tim Dezutter <tim.dezutter@feops.com>
2021-09-29 14:45:40 +0200

    Added requirements, setup and README for virtualenv installation

---
+++ README.md
@@ -0,0 +1,7 @@
+git clone https://www.github.com/UltimateTimmeh/pupsquad.git <EPIONE_ROOT>
+cd <EPIONE_ROOT>
+
+virtualenv -ppython3 .venv
+source venv/bin/activate
+
+pip install -e .
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""PupSquad setup."""
+import setuptools
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as req_file:
+    requirements = req_file.read()
+
+version = "0.0.1"
+setuptools.setup(
+    author="Tim Dezutter",
+    author_email="dezutter.tim@feops.com",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="PupSquad, a platformer for young kids.",
+    install_requires=requirements,
+    long_description=readme+"\n",
+    include_package_data=True,
+    keywords="pupsquad",
+    name="pupsquad",
+    packages=setuptools.find_packages(),
+    url="https://www.github.com/UltimateTimmeh/pupsquad",
+    zip_safe=False,
+    version=version,
+)
